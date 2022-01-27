5eee5706a58378dfafbdd8469d9e366975286e2e
MarynaIlchenko <debovamail@gmail.com>
2022-01-27 16:33:22 +0200

    video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,14 @@
 import Vimeo from '@vimeo/player'
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
+const onPlay = function (data) {
+
+}
+player.on('timeupdate', onPlay);
+localStorage.setItem('videoplayer-current-time', JSON.stringify({}));
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedTime = JSON.parse(savedTime);
+console.log(parsedTime);
