d0aae643e7e19693d660ed6cfd76cb12615033f0
MarynaIlchenko <debovamail@gmail.com>
2022-01-27 14:01:56 +0200

    import simpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
+import simpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const imagesContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+imagesContainer.addEventListener('click', onImagesContainerClick);
+
+function createImageCardsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `      
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}" >
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </div>
+      `
+    }).join('');
+}
+
+function onImagesContainerClick(e) {
+    const isGalleryItemEl = e.target.classList.contains('gallery__image');
+    const imageSrc = e.target.dataset.source;
+    e.preventDefault();
+    if (!isGalleryItemEl) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${imageSrc}" width="800" height="600">`, {
+        onShow: () => {
+            window.addEventListener("keydown", closeByEscButton)
+        },
+        onClose: () => {
+            window.removeEventListener("keydown", closeByEscButton)
+        },
+    });
+   
+function closeByEscButton(e) {
+        if (e.code === 'Escape') {
+            instance.close();
+        };
+    };
+    instance.show();
+}
